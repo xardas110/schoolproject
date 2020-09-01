800f42f2c5ecbeb507369c35c720ba586713fd18
xardas110 <xardas110@yahoo.no>
2020-09-01 14:31:36 +0200

    last commit

--- schoolproject/schoolproject.cpp
+++ schoolproject/schoolproject.cpp
@@ -3,13 +3,22 @@
 
 #include <iostream>
 
+
 using namespace std;
 int main()
 {
+    //can also use unsigned short, but cba.
     short age;
+    short ageLimit = 20;
+    short maxAge = 150;
     cout << "What is your age? ";
     cin >> age;
-    if (age >= (short)20)
+    if (age > maxAge or age < 0)
+    { 
+        cout << "Invalid input";
+            return -1;
+    }
+    if (age >= ageLimit)
         cout << "You are an adult";
     else
         cout << "You are a teenager";
